c0ee8766e9f0f588fe3c4d0f27c50e28196c9d0b
lukakerr <lukakerr1@gmail.com>
2017-10-24 17:40:15 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# swift-macos-sidebar
+
+A simple demonstration of a Notes/Mail style sidebar for MacOS in swift
+
+<p align="center">
+  <img src="https://i.imgur.com/rQfORrG.png">
+</p>
---
+++ swift-macos-sidebar/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  swift-simple-sidebar
+//
+//  Created by Luka Kerr on 24/10/17.
+//  Copyright © 2017 Luka Kerr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+}
+
